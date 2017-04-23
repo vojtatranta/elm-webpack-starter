8bfe731520f8d4d27aa132d98a7902325ad445bd
Vojtěch Tranta <vojta.tranta@gmail.com>
2017-04-23 03:34:22 +0200

    fuck yeah 2048 in Elm

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,170 @@
 module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+import Array exposing (..)
+import Html.Events exposing (onClick)
+
 
 -- component import example
-import Components.Hello exposing ( hello )
+
+import Components.Hello exposing (hello)
 
 
 -- APP
-main : Program Never Int Msg
-main =
-  Html.beginnerProgram { model = model, view = view, update = update }
-
-
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Cell =
+    { value : Int
+    , x : Int
+    , y : Int
+    }
+
+
+type alias Model =
+    { dimension : Int
+    , cells : List Cell
+    }
+
+
+model : Model
+model =
+    { dimension = 4
+    , cells =
+        [ { value = 10, x = 0, y = 0 }
+        , { value = 20, x = 2, y = 0 }
+        , { value = 30, x = 3, y = 0 }
+        ]
+    }
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | Increment
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        Increment ->
+            model
+
+        _ ->
+            model
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+moveRightAll grid dimension =
+    grid
+
+
+getYes : Int -> Array (Array (Maybe Cell)) -> Array (Maybe Cell)
+getYes y arr =
+    let
+        res =
+            Array.get y arr
+    in
+        case res of
+            Just res ->
+                res
+
+            Nothing ->
+                Array.fromList []
+
+
+filterNothings : Array (Maybe Cell) -> Array Cell
+filterNothings arr =
+    (Array.fromList
+        (List.filterMap
+            identity
+            (Array.toList arr)
+        )
+    )
+
+
+cellsToGrid cells dimension =
+    let
+        maybeGrid =
+            (List.foldl
+                (\cell ->
+                    (\arr ->
+                        (Array.set cell.x (Array.set cell.y (Just cell) (getYes cell.y arr)) arr)
+                    )
+                )
+                (Array.repeat dimension (Array.repeat dimension Nothing))
+                cells
+            )
+    in
+        (Array.map
+            filterNothings
+            maybeGrid
+        )
+
+
+moveRight model =
+    model
+
+
+getDims dimension =
+    List.map (\num -> num) (List.range 0 (dimension - 1))
+
+
+getCell grid x y =
+    let
+        cellArray =
+            Array.get x grid
+    in
+        case cellArray of
+            Just yArray ->
+                Array.get y yArray
+
+            Nothing ->
+                Nothing
+
+
+getCellView grid x y =
+    let
+        cell =
+            getCell grid x y
+    in
+        case cell of
+            Just cell ->
+                strong [] [ text (toString cell.value) ]
+
+            Nothing ->
+                text ""
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
+    let
+        grid =
+            cellsToGrid model.cells model.dimension
 
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+        dims =
+            getDims model.dimension
+    in
+        table [ onClick Increment ]
+            [ thead [] (List.concat [ [ (th [] []) ], (List.map (\x -> (th [] [ text (toString (x + 1)) ])) dims) ])
+            , tbody []
+                (List.map
+                    (\y ->
+                        tr []
+                            (List.concat
+                                [ [ td [ class "first" ] [ (text (toString (y + 1))) ] ]
+                                , (List.map (\x -> td [] [ getCellView grid x y ]) dims)
+                                ]
+                            )
+                    )
+                    dims
+                )
+            ]
